7aab6063376c91a47d659241038ee073bd6cbe9a
Anciety <ancietyding@tencent.com>
2022-12-26 11:34:08 +0800

    pure rust implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,123 +1,148 @@
-#![feature(new_uninit)]
+#![feature(new_uninit, allocator_api)]
+use backtrace::{BytesOrWideString, Frame, Symbol};
+use hashbrown::hash_map::DefaultHashBuilder;
+use hashbrown::HashMap;
 use heapless::String as HeaplessString;
-use once_cell::sync::OnceCell;
+use heapless::Vec as HeaplessVec;
+use once_cell::sync::{Lazy, OnceCell};
 use spin::Mutex;
 use std::alloc::{GlobalAlloc, Layout, System};
-use std::ffi::c_void;
+use std::fmt::Display;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering;
+use widestring::U16Str;
 
-type EnumProc = unsafe extern "C" fn(
-    usr_data: *const c_void,
-    address: usize,
-    size: usize,
-    stack: *const usize,
-) -> i32;
+#[derive(Debug, Clone)]
+pub struct Call {
+    pub name: Option<HeaplessString<500>>,
+    pub filename: Option<HeaplessString<500>>,
+    pub line: Option<u32>,
+    pub col: Option<u32>,
 
-extern "C" {
-    fn alloc_internal_init(stack_size: usize);
-    fn alloc_internal_alloc(address: usize, size: usize, stack: *const usize);
-    fn alloc_internal_dealloc(address: usize);
-    fn alloc_enum(usr_data: *const c_void, cb: EnumProc);
+    pub addr: usize,
 }
 
-pub struct LeakTracer<const STACK_SIZE: usize> {
-    backtrace_lock: OnceCell<Mutex<()>>, // we can't use backtrace internal locker, cause it need "std" feature which are not allowed
+impl From<&Symbol> for Call {
+    fn from(value: &Symbol) -> Self {
+        let addr = value.addr().unwrap() as usize;
+        let line = value.lineno();
+        let col = value.colno();
+        let name = value
+            .name()
+            .map(|x| HeaplessString::from(x.as_str().unwrap()));
+        let filename = value.filename_raw().map(|x| match x {
+            BytesOrWideString::Bytes(bytes) => {
+                HeaplessString::from(std::str::from_utf8(bytes).unwrap())
+            }
+            BytesOrWideString::Wide(bytes) => {
+                let mut filename = HeaplessString::default();
+                for c in U16Str::from_slice(bytes).chars_lossy() {
+                    filename.push(c).unwrap();
+                }
+                filename
+            }
+        });
+
+        Self {
+            name,
+            filename,
+            line,
+            col,
+            addr,
+        }
+    }
 }
+
+#[derive(Debug, Clone)]
+pub struct AllocationRecord<const STACK_SIZE: usize> {
+    pub size: usize,
+    pub ptr: usize,
+    pub stack: HeaplessVec<Call, STACK_SIZE>,
+}
+
+impl<const STACK_SIZE: usize> Display for AllocationRecord<STACK_SIZE> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Allocation@{:x} (size {}):\n", self.ptr, self.size)?;
+        for s in self.stack.iter() {
+            let name = s.name.clone().unwrap_or(HeaplessString::from("[unknown]"));
+            let addr = s.addr;
+            let filename = s
+                .filename
+                .clone()
+                .unwrap_or(HeaplessString::from("[unknown file]"));
+            write!(f, "  {name} {addr:x} @ {filename}")?;
+            match (s.line, s.col) {
+                (Some(line), Some(col)) => write!(f, ":{line}-{col})")?,
+                (Some(line), _) => write!(f, ":{line}")?,
+                // What about col present but missing line?
+                // Normally this should not happen, so it should be safe to ignore that.
+                (_, _) => {}
+            };
+            write!(f, "\n")?;
+        }
+        Ok(())
+    }
+}
+
+pub struct LeakTracerInner<const STACK_SIZE: usize> {
+    allocates: Mutex<HashMap<usize, AllocationRecord<STACK_SIZE>, DefaultHashBuilder, System>>,
+    enabled: AtomicBool,
+}
+
+impl<const STACK_SIZE: usize> Default for LeakTracerInner<STACK_SIZE> {
+    fn default() -> Self {
+        Self {
+            allocates: Mutex::new(HashMap::default()),
+            enabled: AtomicBool::new(true),
+        }
+    }
+}
+
+pub struct LeakTracer<const STACK_SIZE: usize>(Lazy<LeakTracerInner<STACK_SIZE>>);
 
 pub type LeakTracerDefault = LeakTracer<10>;
 
 impl<const STACK_SIZE: usize> LeakTracer<STACK_SIZE> {
     pub const fn new() -> Self {
-        LeakTracer {
-            backtrace_lock: OnceCell::new(),
-        }
-    }
-    pub fn init(&self) {
-        assert!(STACK_SIZE > 0);
-        self.backtrace_lock.set(Mutex::new(())).ok();
-        unsafe {
-            alloc_internal_init(STACK_SIZE);
-        }
+        Self(Lazy::new(|| LeakTracerInner::default()))
     }
 
-    extern "C" fn alloc_enum_cb(
-        usr_data: *const c_void,
-        address: usize,
-        size: usize,
-        stack: *const usize,
-    ) -> i32 {
-        let closure: &mut &mut dyn FnMut(usize, usize, &[usize]) -> bool =
-            unsafe { std::mem::transmute(usr_data) };
+    pub fn get_leaks(
+        &self,
+    ) -> HashMap<usize, AllocationRecord<STACK_SIZE>, DefaultHashBuilder, System> {
+        self.0.enabled.store(false, Ordering::SeqCst);
 
-        let s = unsafe { std::slice::from_raw_parts(stack, STACK_SIZE) };
-        if closure(address, size, s) {
-            1
-        } else {
-            0
+        let mut out = HashMap::default();
+        for (k, v) in self.0.allocates.lock().iter() {
+            out.insert(*k, v.clone());
         }
-    }
 
-    pub fn now_leaks<F>(&self, mut callback: F)
-    where
-        F: FnMut(usize, usize, &[usize]) -> bool,
-    {
-        // https://stackoverflow.com/questions/32270030/how-do-i-convert-a-rust-closure-to-a-c-style-callback
-        let mut cb: &mut dyn FnMut(usize, usize, &[usize]) -> bool = &mut callback;
-        let cb = &mut cb;
-        unsafe {
-            alloc_enum(cb as *const _ as *const c_void, Self::alloc_enum_cb);
-        }
-    }
+        self.0.enabled.store(true, Ordering::SeqCst);
 
-    pub fn get_symbol_name(&self, addr: usize) -> Option<String> {
-        let mut ret: Option<String> = None;
-        if let Some(locker) = self.backtrace_lock.get() {
-            let mut symbol_buf = {
-                // some symbol name really long, so alloc 500 bytes to hold
-                let alloc_on_heap = Box::<HeaplessString<500>>::new_zeroed();
-                unsafe { alloc_on_heap.assume_init() }
-            };
-            let mut got = false;
-            let l = locker.lock();
-            unsafe {
-                backtrace::resolve_unsynchronized(addr as *mut _, |symbol| {
-                    // do NOT trigger alloc in closure scope!
-                    match symbol.name() {
-                        Some(x) => {
-                            use std::fmt::Write;
-                            write!(&mut symbol_buf, "{}", x).ok();
-                            got = true;
-                        }
-                        _ => {}
-                    }
-                });
-            }
-            drop(l);
-            if got {
-                ret = Some(symbol_buf.as_str().to_owned());
-            }
-        }
-        ret
+        out
     }
 
     fn alloc_accounting(&self, size: usize, ptr: *mut u8) -> *mut u8 {
-        let locker = if let Some(l) = self.backtrace_lock.get() {
-            l
-        } else {
+        if !self.0.enabled.load(Ordering::SeqCst) {
             return ptr;
-        };
-        let mut vs = [0usize; STACK_SIZE];
-        let l = if cfg!(os = "windows") {
-            Some(locker.lock())
-        } else {
-            None
-        };
+        }
+
+        let mut stack = HeaplessVec::default();
         let mut count = 0;
+        // First 2 stack is in the closure itself, meaningless, skip that.
+        let mut skip_count = 2;
         // On win7 64, it's may cause deadlock, solution is to palce a newer version of dbghelp.dll combined with exe
         unsafe {
             backtrace::trace_unsynchronized(|frame| {
-                let symbol_address = frame.ip();
-                vs[count] = symbol_address as usize;
-                count += 1;
+                if skip_count > 0 {
+                    skip_count -= 1;
+                    return true;
+                }
+
+                backtrace::resolve_frame_unsynchronized(frame, |symbol| {
+                    stack.push(symbol.into()).unwrap();
+                    count += 1;
+                });
                 if count >= STACK_SIZE {
                     false
                 } else {
@@ -125,17 +150,26 @@ impl<const STACK_SIZE: usize> LeakTracer<STACK_SIZE> {
                 }
             });
         }
-        drop(l);
-        unsafe {
-            alloc_internal_alloc(ptr as usize, size, vs.as_ptr());
-        }
+
+        let allocation_record = AllocationRecord {
+            size,
+            ptr: ptr as usize,
+            stack,
+        };
+        self.0
+            .allocates
+            .lock()
+            .insert(ptr as usize, allocation_record);
+
         ptr
     }
 
     fn dealloc_accounting(&self, ptr: *mut u8) {
-        unsafe {
-            alloc_internal_dealloc(ptr as usize);
+        if !self.0.enabled.load(Ordering::SeqCst) {
+            return;
         }
+
+        self.0.allocates.lock().remove(&(ptr as usize));
     }
 }
 
